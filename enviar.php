cedba79df201859623989aa13c14f497b290085d
lisandro <lisandrodiomedes@gmail.com>
2019-08-27 10:12:58 -0400

    contacto de prueba

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php
+
+$destino = "lisandrodiomedes@gmail.com";
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+$contenido = "nombre" . $nombre . "\ncorreo" . $correo . "\ntelefono" . $telefono . "\nmensaje" . $mensaje; 
+mail($destino, "Contenido", $contenido);
+?>
