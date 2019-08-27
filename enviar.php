df84f235eee71386cc89e99949b9ee749407f243
lisandro <lisandrodiomedes@gmail.com>
2019-08-27 11:01:15 -0400

    trabajo modificado

--- enviar.php
+++ enviar.php
@@ -5,6 +5,7 @@ $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
-$contenido = "nombre" . $nombre . "\ncorreo" . $correo . "\ntelefono" . $telefono . "\nmensaje" . $mensaje; 
+$contenido = "nombre" . $nombre . "\ncorreo" . $correo . "\ntelefono" . $telefono . "\nmensaje" . $mensaje;
+header('location:index.html'); 
 mail($destino, "Contenido", $contenido);
 ?>
